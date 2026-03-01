3bc825a2e57ffe83d4bb72064559c8b70375b400
Demonicky <edotank47@gmail.com>
2026-03-01 11:59:19 +0700

    add new project to git

---
+++ src/main/java/com/dobudobu/learn_chinese/LearnChineseApplication.java
@@ -0,0 +1,13 @@
+package com.dobudobu.learn_chinese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnChineseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnChineseApplication.class, args);
+	}
+
+}
